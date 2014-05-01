4fdf161738c7c5d320853a7e8a467da3af70a891
lalyos <lalyos@yhoo.com>
2014-05-01 14:00:04 +0200

    bootstrap doesn't need parameters as it is run as CMD instead of ENTRYPOINT

--- ambari-server/bootstrap.sh
+++ ambari-server/bootstrap.sh
@@ -3,10 +3,6 @@
 ambari-agent start
 ambari-server start
 
-if [[ $1 == "-d" ]]; then
-  while true; do sleep 1000; done
-fi
-
-if [[ $1 == "-bash" ]]; then
-  /bin/bash
-fi
+while true; do
+   tail -f /var/log/ambari-*/ambari-*.log
+done
