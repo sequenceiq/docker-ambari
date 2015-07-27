79eb6491c33e1f8edca81c522ffeaab80dc4c13f
Richard Kovacs <kovacsricsi at gmail>
2015-07-27 12:13:02 +0200

    Replace blueprint versions to 2.3

--- ambari-server/install-cluster.sh
+++ ambari-server/install-cluster.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ./ambari-shell.sh << EOF
-blueprint defaults
+blueprint add --url https://raw.githubusercontent.com/sequenceiq/ambari-rest-client/2.1.11/src/main/resources/blueprints/multi-node-hdfs-yarn
+blueprint add --url https://raw.githubusercontent.com/sequenceiq/ambari-rest-client/2.1.11/src/main/resources/blueprints/single-node-hdfs-yarn
 cluster build --blueprint $BLUEPRINT
 cluster autoAssign
 cluster create --exitOnFinish true
