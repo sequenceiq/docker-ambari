ec1652d18846abff54daff8b1ed797dd7c74b69a
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2015-11-13 15:52:00 +0100

    fix metadata issue on Azure

--- ambari-server/public-hostname.sh
+++ ambari-server/public-hostname.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-curl -s -m 3 http://169.254.169.254/latest/meta-data/public-ipv4 || dig +short myip.opendns.com @resolver1.opendns.com
+STATUS=$(curl -s -m 5 -w "%{http_code}" http://169.254.169.254/latest/meta-data/public-ipv4 -o /dev/null)
+if [ "$STATUS" == "200" ]; then
+	curl -s -m 5 http://169.254.169.254/latest/meta-data/public-ipv4 
+else
+	dig +short myip.opendns.com @resolver1.opendns.com
+fi
