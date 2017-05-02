d6e8d466a2dc7c692d85cac8486b6a13b7e8ba21
KARTHICK Duraisamy Soundararaj <ksoundararaj@karthick-work.group.on>
2017-05-02 12:09:44 -0700

    Changes to decouple nameserver (re)configuration

--- ambari-agent/docker_shared/init-agent.sh
+++ ambari-agent/docker_shared/init-agent.sh
@@ -10,29 +10,6 @@ debug() {
   [[ "DEBUG" ]]  && echo "[DEBUG] $@" 1>&2
 }
 
-get_nameserver_addr() {
-  if [[ "$NAMESERVER_ADDR" ]]; then
-    echo $NAMESERVER_ADDR
-  else
-    if ip addr show docker0 &> /dev/null; then
-      ip addr show docker0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1
-    else
-      ip ro | grep default | cut -d" " -f 3
-    fi
-  fi
-}
-
-# --dns isn't available for: docker run --net=host
-# sed -i /etc/resolf.conf fails:
-# sed: cannot rename /etc/sedU9oCRy: Device or resource busy
-# here comes the tempfile workaround ...
-local_nameserver() {
-  cat>/etc/resolv.conf<<EOF
-nameserver $(get_nameserver_addr)
-search service.consul node.dc1.consul
-EOF
-}
-
 ambari_server_addr() {
   sed -i "s/^hostname=.*/hostname=${AMBARI_SERVER_ADDR}/" /etc/ambari-agent/conf/ambari-agent.ini
 }
@@ -46,6 +23,9 @@ reorder_dns_lookup() {
 }
 
 main() {
+  ln -s /docker_shared/etc/resolv.conf /tmp/resolv.conf
+  cp /tmp/resolv.conf /etc/resolv.conf
+
   [[ "$USE_CONSUL_DNS" == "true" ]] && local_nameserver
   ambari_server_addr
   reorder_dns_lookup
